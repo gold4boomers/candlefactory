7d4d3da6cf5f95da144a61a128fc1a56bc151aa2
Gold 4 Boomers <77053346+gold4boomers@users.noreply.github.com>
2021-01-15 20:34:11 +0100

    initial commit
    001 - add skeleton files and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # candlefactory
 scripts to download a lot of candle data from cryptocurrency exchanges
+
+python3.8 -m venv .direnv
+source .direnv/bin/activate
+pip install -r candlefactory/requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+    name='CandleFactory',
+    version='0.1dev',
+    packages=['candlefactory',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
