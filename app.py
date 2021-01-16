2a28607b2fc0abd169ff87b2a0411d7fe29847de
Gold 4 Boomers <77053346+gold4boomers@users.noreply.github.com>
2021-01-16 04:56:20 +0100

    Update app.py
    it works

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ async def multi_markets(exchanges):
     markets = await asyncio.gather(*input_coroutines, return_exceptions=True)
     return markets
 
-def single_ohclv(exchange_id, symbol, since, timeframe):
+def single_ohclv(exchange_id, symbol, quoteid, baseid, since, timeframe):
     data = []
     limit = 100
     exchange = getattr(ccxt, exchange_id)({'enableRateLimit': True})
@@ -60,18 +60,28 @@ def single_ohclv(exchange_id, symbol, since, timeframe):
             print('Got an error', type(error).__name__, error.args, ', retrying in', hold, 'seconds...')
             time.sleep(hold)
 
-def asingle_ohclv(exchange_id, symbol, quoteId, baseId, since, timeframe):
+async def asingle_ohclv(ex):
+    symbol = ex['symbol']
+    quoteId = ex['quoteId']
+    baseId = ex['baseId']
+    since = ex['since']
+    timeframe = ex['timeframe']
+    exchange_id = ex['id']
+
     data = []
     limit = 100
-    exchange = getattr(ccxt, exchange_id)({'enableRateLimit': True})
     filename = f'{quoteId}_{baseId}-{timeframe}-{exchange_id}.csv'
+    exchange = getattr(ccxta, exchange_id)({'enableRateLimit': True})
+
     timeframe_duration_in_seconds = exchange.parse_timeframe(timeframe)
     timeframe_duration_in_ms = timeframe_duration_in_seconds * 1000
     from_timestamp = exchange.parse8601(since)
     now = exchange.milliseconds()
+    await exchange.close()
     while from_timestamp <= now:
+        exchange = getattr(ccxta, exchange_id)({'enableRateLimit': True})
         try:
-            ohlcvs = exchange.fetch_ohlcv(symbol, timeframe, from_timestamp, limit=limit)
+            ohlcvs = await exchange.fetch_ohlcv(symbol, timeframe, from_timestamp, limit)
             if len(ohlcvs) > 0:
                 first = ohlcvs[0][0]
                 last = ohlcvs[-1][0]
@@ -81,14 +91,17 @@ def asingle_ohclv(exchange_id, symbol, quoteId, baseId, since, timeframe):
                 data += ohlcvs
         except (ccxt.ExchangeError, ccxt.AuthenticationError, ccxt.ExchangeNotAvailable, ccxt.RequestTimeout) as error:
             print('Got an error', type(error).__name__, error.args, ', retrying in', hold, 'seconds...')
-            time.sleep(hold)
-    with open(f'out/{filename}', mode='w') as output_file:
-        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerows(data)
-    return True
+            return
+        finally:
+            time.sleep(1)
+            await exchange.close()
+            break
+    if data:
+        return {'filename': filename, 'data': data, 'base': baseId }
 
-async def multi_ohlcv(list_exchange_symbol, since, timeframe):
-    input_coroutines = [asingle_ohclv(k['id'], k['symbol'], k['quoteId'], k['baseId'], since, timeframe) for k in list_exchange_symbol]
+
+async def multi_ohlcv(list_exchange_symbol):
+    input_coroutines = [asingle_ohclv(k) for k in list_exchange_symbol]
     markets = await asyncio.gather(*input_coroutines, return_exceptions=True)
     return markets
 
@@ -97,10 +110,11 @@ if __name__ == '__main__':
     timeframe = '1d'
     since='2020-01-01T00:00:00Z'
 
-    exchanges = ["kucoin", "bittrex", "bitfinex", "poloniex","huobi","coinbasepro","okcoinusd", "huobipro", "binance", "coinbase"]
+    exchanges = ["binance", "kucoin", "poloniex", "hitbtc", "bittrex", "coinbase", "coinbaseprime", "coinbasepro", "huobipro", "okex"]
 
     tic = time.time()
     a = asyncio.get_event_loop().run_until_complete(multi_markets(exchanges))
+
 
     results = []
     i = 0
@@ -111,22 +125,40 @@ if __name__ == '__main__':
 
         tic = time.time()
         for ex in a:
-            exchange_id = ex['exchange']
+            try:
+                exchange_id = ex['exchange']
+            except:
+                print(ex)
+                continue
             for coin, coindata in ex['markets'].items():
+                symbol = coindata['symbol']
                 baseId = coindata['baseId'].upper()
                 quoteId = coindata['quoteId'].upper()
                 if quoteId == 'BTC' and baseId != 'BTC' and baseId not in results and baseId not in list_base:
-                    list_exchange_symbol.append({'id': exchange_id, 'symbol': coindata['id'], 'quote': quoteId, 'base': baseId})
-                    list_base.append(baseId)
+                    list_exchange_symbol.append({'id': exchange_id, 
+                    'symbol': symbol, 
+                    'quoteId': quoteId, 
+                    'baseId': baseId, 
+                    'since': since, 
+                    'timeframe': timeframe})
                     ex['markets'].pop(coin)
                     break
         
-        multi_ohlcv(list_exchange_symbol, since, timeframe)
-        list_exchange_symbol = []
+        b = asyncio.get_event_loop().run_until_complete(multi_ohlcv(list_exchange_symbol))
+        for res in b:
+            if res == None:
+                continue
+            if not 'filename' in res.keys():
+                print('no fname')
+                continue
+            if not 'data' in res.keys():
+                print('no data')
+                continue
+            filename = res['filename']
+            data = res['data']
+            with open(f'out/{res["filename"]}', mode='w') as output_file:
+                csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerows(res["data"])
+            list_base.append(res['base'])
         results += list_base
-        print(results)
-        if i >= 10:
-            break
-
     print("async call spend:", time.time() - tic)
-
